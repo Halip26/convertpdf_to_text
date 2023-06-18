a744ff9110869bfa2abfaf2006c640402649a71a
Halip26 <halipuddin.angko@gmail.com>
2023-06-19 07:11:51 +0800

    it works now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # create code that can be convert pdf file to txt
 import os
+import warnings
 from pdfreader import SimplePDFViewer
 
 # Create directory if it doesn't exist
@@ -7,10 +8,13 @@ if not os.path.exists("hasil"):
     os.makedirs("hasil")
 
 # Open the PDF file in read-binary mode
-with open("sample.pdf", 'rb') as pdf_file:
+with open("./pdf_file/contoh.pdf", "rb") as pdf_file:
 
     # Create a PDF viewer object
     pdf_viewer = SimplePDFViewer(pdf_file)
+
+    # Filter out the binary data warning
+    warnings.filterwarnings("ignore", category=Warning)
 
     # Iterate through each page in the PDF document
     for idx, canvas in enumerate(pdf_viewer):
@@ -22,5 +26,5 @@ with open("sample.pdf", 'rb') as pdf_file:
         output_name = f"hasil/example-page{idx}.txt"
 
         # Write extracted text to txt file
-        with open(output_name, 'w') as txt_file:
+        with open(output_name, "w") as txt_file:
             txt_file.write(page_text)
