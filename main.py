30a1b5ef9d71ee0cfac8f8d21595e14936daef92
Halip26 <halipuddin.angko@gmail.com>
2023-06-19 07:30:33 +0800

    update & delete unneccesary files

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 # create code that can be convert pdf file to txt
 import os
+import sys
 import warnings
 from pdfreader import SimplePDFViewer
+
+# cek apakah direktori sudah ada
+if len(sys.argv) < 2:
+    print("Usage: python main.py <pdf_file>")
+    sys.exit(1)
+
+# get pdf filename from command line argument
+pdf_file_name = sys.argv[1]
 
 # Membuat direktori jika belum ada
 if not os.path.exists("hasil"):
     os.makedirs("hasil")
 
 # Membuka file PDF dengan mode baca biner
-with open("./pdf_file/contoh.pdf", "rb") as pdf_file:
+with open(pdf_file_name, "rb") as pdf_file:
 
-    # Membuat objek SimplePDFViewer
+    # create SimplePDFViewer object
     pdf_viewer = SimplePDFViewer(pdf_file)
 
     # Filtering warning data binary
@@ -23,7 +32,7 @@ with open("./pdf_file/contoh.pdf", "rb") as pdf_file:
         page_text = "".join(canvas.strings)
 
         # Menentukan nama file hasil
-        output_name = f"hasil/example-page{idx}.txt"
+        output_name = f"hasil/{os.path.splitext(os.path.basename(pdf_file_name))[0]}-page{idx}.txt"
 
         # Menulis teks hasil ekstraksi ke dalam file teks
         with open(output_name, "w") as txt_file:
